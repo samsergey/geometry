daa5ab0f4137774e4a603eea3e3b56d2a42b016e
Sergey at ssb <samsergey@yandex.ru>
2020-05-08 08:31:25 +1200

    minor changes

--- emacs-settings.el
+++ emacs-settings.el
@@ -1,3 +1,8 @@
+;; add lines to .emacs
+;; (load "~/work/webinar/webfigs/livescript-mode.el")
+;; (load "~/work/webinar/webfigs/emacs-settings.el")
+
+
 (add-to-list 'auto-mode-alist '("\\.ls\\'" . livescript-mode))
 (add-hook 'livescript-mode-hook `hasklig-mode)
 (add-hook 'livescript-mode-hook 'livescript-unicode)
